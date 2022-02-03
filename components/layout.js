f70a18dc73581b90444e06958dae4cc83eee36ea
Vishnu Vijayan <vishnu08ptb@gmail.com>
2022-02-03 22:00:18 +0530

    feat: mobile view

--- components/layout.js
+++ components/layout.js
@@ -1,48 +1,103 @@
-import Link from 'next/link';
-import { useRouter } from 'next/router';
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import {
+  ListItemText,
+  Box,
+  Grid,
+  Divider,
+  Collapse,
+  IconButton,
+} from "@mui/material";
+import { MenuOutlined, CloseOutlined } from "@mui/icons-material";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Image from "next/image";
 
 export default function Layout({ children }) {
   const router = useRouter();
+  const [visible, setVisible] = React.useState(false);
+  const showDrawer = () => {
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
+
+  const list = () => (
+    <Box>
+      <List>
+        <Link href="/">
+          <ListItem className="cursor-pointer">
+            <ListItemText primary="Home" />
+          </ListItem>
+        </Link>
+        <Divider />
+        <Link href="/marketwise">
+          <ListItem className="cursor-pointer">
+            <ListItemText primary="Marketwise" />
+          </ListItem>
+        </Link>
+        <Divider />
+        <Link href="/keepthekidsbusy">
+          <ListItem className="cursor-pointer">
+            <ListItemText primary="Keep the kids busy" />
+          </ListItem>
+        </Link>
+        <Divider />
+        <Link href="/about">
+          <ListItem className="cursor-pointer">
+            <ListItemText primary="About" />
+          </ListItem>
+        </Link>
+        <Divider />
+        <Link href="/contact">
+          <ListItem className="cursor-pointer">
+            <ListItemText primary="Contact" />
+          </ListItem>
+        </Link>
+        <Divider />
+      </List>
+    </Box>
+  );
 
   const menuItems = [
     {
-      href: '/',
-      title: 'Home',
+      href: "/",
+      title: "Home",
     },
     {
-        href: '/marketwise',
-        title: 'MarketWise',
-      },
-      {
-        href: '/keepthekidsbusy',
-        title: 'Keep the Kids Busy',
-      },
-    {
-      href: '/about',
-      title: 'About',
+      href: "/marketwise",
+      title: "MarketWise",
     },
     {
-      href: '/contact',
-      title: 'Contact',
+      href: "/keepthekidsbusy",
+      title: "Keep the Kids Busy",
     },
-
+    {
+      href: "/about",
+      title: "About",
+    },
+    {
+      href: "/contact",
+      title: "Contact",
+    },
   ];
 
   return (
-    
-    <div className='flex flex-col flex-grow w-full min-h-screen my-20 md:flex md:flex-row'>
-
-
-      <div className='flex flex-col flex-1 mx-20 my-50 md:flex-row'>
-        <aside className='w-60 bg-white-100 md:w-50'>
+    <div className="flex flex-col flex-grow w-full min-h-screen my-5 md:my-20 md:flex md:flex-row">
+      <div className="flex flex-col flex-1 mx-3  md:mx-20 my-50 md:flex-row">
+        <aside className="hidden md:block bg-white-100">
           <nav>
             <ul>
               {menuItems.map(({ href, title }) => (
-                <li className='m-2' key={title}>
+                <li className="m-2" key={title}>
                   <Link href={href}>
                     <a
                       className={`flex p-2 bg-white-200 rounded hover:bg-slate-300 font-semibold cursor-pointer ${
-                        router.asPath === href && 'bg-white-600 text-black'
+                        router.asPath === href && "bg-white-600 text-black"
                       }`}
                     >
                       {title}
@@ -53,9 +108,57 @@ export default function Layout({ children }) {
             </ul>
           </nav>
         </aside>
-        <main className='flex-1'>{children}</main>
-      </div>
+        <div className="block md:hidden">
+          <Grid
+            // className=
+            container
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Grid item sm={6}>
+              <div
+                style={{
+                  // position: "relative",
+                  width: 150,
+                  height: 70,
+                }}
+                className="relative w-150 h-75"
+              >
+                <Image
+                  src={"/logo.png"}
+                  objectFit="contain"
+                  alt="Genrobotics-Logo"
+                  layout="fill"
+                />
+              </div>
+            </Grid>
 
+            <Grid item sm={6}>
+              {!visible && (
+                <span className="trigger-menu">
+                  <IconButton onClick={showDrawer}>
+                    <MenuOutlined />
+                  </IconButton>
+                </span>
+              )}
+            </Grid>
+
+            <Drawer
+              PaperProps={{
+                sx: { width: "50%" },
+              }}
+              anchor="left"
+              open={visible}
+              onBackdropClick={onClose}
+              variant="temporary"
+            >
+              {list()}
+            </Drawer>
+          </Grid>
+        </div>
+        <main className="container">{children}</main>
+      </div>
     </div>
   );
 }
--- pages/marketwise.js
+++ pages/marketwise.js
@@ -1,33 +1,55 @@
-import Link from 'next/link'
-
+import Link from "next/link";
 
 function MarketWise() {
-    return (
-      
-      <div className='flex flex-col h-full mx-10 my-5 font-bold items-left'>
+  return (
+    <div className="flex flex-col h-full mx-10 my-5 font-bold items-left">
+      <h1 className="font-bold text-blue-600 hover:text-blue-800 visited:text-purple-600 text-[25px]">
+        <Link href="https://getMarketwise.io">
+          <a target="_blank">MarketWise</a>
+        </Link>
+      </h1>
 
-        <h1 className = 'font-bold text-blue-600 hover:text-blue-800 visited:text-purple-600 text-[25px]'><Link href = "https://getMarketwise.io"><a target="_blank">MarketWise</a></Link></h1>
-        
-        <h3 className = "my-5 font-semibold">MarketWise is a SaaS application for capital markets and our primary focus !</h3>
-        
-        <p className = "my-5 font-semibold"> MarketWise helps customers stay on top of all their information in the financial markets in one place 
+      <h3 className="my-5 font-semibold">
+        MarketWise is a SaaS application for capital markets and our primary
+        focus !
+      </h3>
 
-         On top of looking at stock, bond and other financial market data. Customers can create and access custom data. 
-        
- For example, the customer can request their counterpart to share valuation of a specific trade or inflation forecasts through the api or the excel addin. They can then instantly track this on the webapp </p>
+      <p className="my-5 font-semibold text-justify">
+        {" "}
+        MarketWise helps customers stay on top of all their information in the
+        financial markets in one place On top of looking at stock, bond and
+        other financial market data. Customers can create and access custom
+        data. For example, the customer can request their counterpart to share
+        valuation of a specific trade or inflation forecasts through the api or
+        the excel addin. They can then instantly track this on the webapp{" "}
+      </p>
 
-        <p className ="my-5 font-semibold"> The MarketWise Excel Addin can be downloaded from the AppSource <Link  href = "https://appsource.microsoft.com/en-us/product/office/WA200003731?tab=Overview"><a className = "font-bold text-blue-600 hover:text-blue-800 visited:text-purple-600" target ="_blank">here</a></Link></p>
+      <p className="my-5 font-semibold">
+        {" "}
+        The MarketWise Excel Addin can be downloaded from the AppSource{" "}
+        <Link href="https://appsource.microsoft.com/en-us/product/office/WA200003731?tab=Overview">
+          <a
+            className="font-bold text-blue-600 hover:text-blue-800 visited:text-purple-600"
+            target="_blank"
+          >
+            here
+          </a>
+        </Link>
+      </p>
 
-        
-        <p className = "my-5"> Watch the video to find out more !</p>
+      <p className="my-5"> Watch the video to find out more !</p>
 
-        <video playsInline autoPlay controls width="60%"
-            source src="/marketwise-video.mp4" type="video/mp4" />
-        
- 
-      </div>
-    );
-  }
+      <video
+        playsInline
+        autoPlay
+        controls
+        width="60%"
+        source
+        src="/marketwise-video.mp4"
+        type="video/mp4"
+      />
+    </div>
+  );
+}
 
-export default MarketWise
-  
+export default MarketWise;
