332e11c807d0346c181bfeb6039b94cf909b772e
nnechm <m.chennupati@gmail.com>
2022-02-04 13:08:49 +0100

    changes ok

--- components/layout.js
+++ components/layout.js
@@ -1,48 +1,122 @@
-import Link from 'next/link';
-import { useRouter } from 'next/router';
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import {
+  ListItemText,
+  Box,
+  Grid,
+  Divider,
+  IconButton,
+  useMediaQuery,
+} from "@mui/material";
+import { MenuOutlined } from "@mui/icons-material";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Image from "next/image";
 
 export default function Layout({ children }) {
   const router = useRouter();
+  const [visible, setVisible] = React.useState(false);
+  const showDrawer = () => {
+    setVisible(true);
+  };
+  const matches = useMediaQuery("(min-width:992px)");
+
+  const onClose = () => {
+    setVisible(false);
+  };
+
+  const hideDrawer = () => {
+    setVisible(false);
+  };
+
+  const list = () => (
+    <Box>
+      <List>
+        <Link href="/" passHref>
+          <ListItem onClick={hideDrawer} className="cursor-pointer">
+            <ListItemText primary="Home" />
+          </ListItem>
+        </Link>
+        <Divider />
+        <Link href="/marketwise" passHref>
+          <ListItem onClick={hideDrawer} className="cursor-pointer">
+            <ListItemText primary="Marketwise" />
+          </ListItem>
+        </Link>
+        <Divider />
+        <Link href="/keepthekidsbusy" passHref>
+          <ListItem onClick={hideDrawer} className="cursor-pointer">
+            <ListItemText primary="Keep the kids busy" />
+          </ListItem>
+        </Link>
+        <Divider />
+        <Link href="/about" passHref>
+          <ListItem onClick={hideDrawer} className="cursor-pointer">
+            <ListItemText primary="About" />
+          </ListItem>
+        </Link>
+        <Divider />
+        <Link href="/contact" passHref>
+          <ListItem onClick={hideDrawer} className="cursor-pointer">
+            <ListItemText primary="Contact" />
+          </ListItem>
+        </Link>
+        <Divider />
+      </List>
+    </Box>
+  );
 
   const menuItems = [
     {
-      href: '/',
-      title: 'Home',
+      href: "/",
+      title: "Home",
     },
     {
-        href: '/marketwise',
-        title: 'MarketWise',
-      },
-      {
-        href: '/keepthekidsbusy',
-        title: 'Keep the Kids Busy',
-      },
-    {
-      href: '/about',
-      title: 'About',
+      href: "/marketwise",
+      title: "MarketWise",
     },
     {
-      href: '/contact',
-      title: 'Contact',
+      href: "/keepthekidsbusy",
+      title: "Keep the Kids Busy",
     },
-
+    {
+      href: "/about",
+      title: "About",
+    },
+    {
+      href: "/contact",
+      title: "Contact",
+    },
   ];
 
   return (
-    
-    <div className='flex flex-col flex-grow w-full min-h-screen my-20 md:flex md:flex-row'>
-
-
-      <div className='flex flex-col flex-1 mx-20 my-50 md:flex-row'>
-        <aside className='w-60 bg-white-100 md:w-50'>
+    <div className="flex flex-col flex-grow w-full min-h-screen my-5 md:my-20 md:flex md:flex-row">
+      <div className="flex flex-col flex-1 mx-3  md:mx-20 my-50 md:flex-row">
+        <aside className="hidden md:block bg-white-100">
           <nav>
+            <div
+              style={{
+                width: 125,
+                height: 125,
+              }}
+              className="relative ml-2"
+            >
+              <Image
+                src={"/logo.png"}
+                objectFit="contain"
+                alt="Genrobotics-Logo"
+                layout="fill"
+              />
+            </div>
             <ul>
               {menuItems.map(({ href, title }) => (
-                <li className='m-2' key={title}>
+                <li className="m-2" key={title}>
                   <Link href={href}>
                     <a
                       className={`flex p-2 bg-white-200 rounded hover:bg-slate-300 font-semibold cursor-pointer ${
-                        router.asPath === href && 'bg-white-600 text-black'
+                        router.asPath === href && "bg-white-600 text-black"
                       }`}
                     >
                       {title}
@@ -53,9 +127,59 @@ export default function Layout({ children }) {
             </ul>
           </nav>
         </aside>
-        <main className='flex-1'>{children}</main>
-      </div>
+        <div className="block md:hidden">
+          <div
+            className="container flex flex-row align-center grid grid-cols-3"
+            // className=
+            // container
+            // direction="row"
+            // justifyContent="space-between"
+            // alignItems="center"
+          >
+            <div />
+            <div>
+              <div
+                style={{
+                  width: 140,
+                  height: 80,
+                }}
+                className="relative"
+              >
+                <Image
+                  src={"/logo.png"}
+                  objectFit="contain"
+                  alt="Genrobotics-Logo"
+                  layout="fill"
+                />
+              </div>
+            </div>
 
+            <div className="flex justify-end items-center">
+              {!visible && (
+                <span className="trigger-menu">
+                  <IconButton onClick={showDrawer}>
+                    <MenuOutlined />
+                  </IconButton>
+                </span>
+              )}
+            </div>
+            {!matches && (
+              <Drawer
+                PaperProps={{
+                  sx: { width: "50%" },
+                }}
+                anchor="right"
+                open={visible}
+                onBackdropClick={onClose}
+                variant="temporary"
+              >
+                {list()}
+              </Drawer>
+            )}
+          </div>
+        </div>
+        <main className="container">{children}</main>
+      </div>
     </div>
   );
 }
--- pages/keepthekidsbusy.js
+++ pages/keepthekidsbusy.js
@@ -1,33 +1,49 @@
 /* eslint-disable react/no-unescaped-entities */
-import Link from 'next/link'
+import Link from "next/link";
 
 function keepthekidsbusy() {
+  return (
+    <div className="flex flex-col h-full mx-10 my-5 font-bold items-left">
+      <h1 className="font-bold text-blue-600 hover:text-blue-800 visited:text-purple-600 text-[25px]">
+        <Link href="https://keepthekidsbusy.io">
+          <a target="_blank">Keep The Kids Busy !</a>
+        </Link>
+      </h1>
 
-    return (
+      <h3 className="my-5 font-semibold">
+        KtK is a simple events list to bring spontaneity back in a child's life
+        !
+      </h3>
 
-      <div className='flex flex-col h-full mx-10 my-5 font-bold items-left'>
+      <p className="my-5 font-semibold text-justify">
+        {" "}
+        Friends, grandparents or uncles and aunts, Simply create an event that
+        lets the kids know when you are free and what you are doing.{" "}
+      </p>
 
-    <h1 className = 'font-bold text-blue-600 hover:text-blue-800 visited:text-purple-600 text-[25px]'><Link href = "https://keepthekidsbusy.io"><a target="_blank">Keep The Kids Busy !</a></Link></h1>
+      <p className="my-5 font-semibold">
+        Create a simple calendar event using the form - Join in virtually or in
+        real life !{" "}
+      </p>
 
-    <h3 className = "my-5 font-semibold">KtK is a simple events list to bring spontaneity back in  a child's life !</h3>
+      <p className="my-5 font-semibold">
+        {" "}
+        Kids can then look up what's on that day or next and simply join in....{" "}
+      </p>
 
-    <p className = "my-5 font-semibold"> Friends, grandparents or uncles and aunts, Simply create an event that lets the kids know when you are free and what you are doing. </p>
+      <p className="my-5 font-semibold">
+        {" "}
+        Play a computer game together, Read a book or organize a football match
+        !
+      </p>
 
-      <p className = "my-5 font-semibold">Create a simple calendar event using the form  - Join in virtually or in real life ! </p>
-
-      <p className = "my-5 font-semibold"> Kids can then look up what's on that day or next and simply join in.... </p>
-
-      <p className = "my-5 font-semibold"> Play a computer game together, Read a book or organize a football match !</p>
-
-     <Link href="https://keepthekidsbusy.io"><a target ="_blank"><img  src='/ktk.png' alt='logo'/></a></Link>
-
-
-      
-
-
-</div>
-)
+      <Link href="https://keepthekidsbusy.io">
+        <a target="_blank">
+          <img src="/ktk.png" alt="logo" />
+        </a>
+      </Link>
+    </div>
+  );
 }
- 
 
-export default keepthekidsbusy 
+export default keepthekidsbusy;
